0c7f0f7d854be315718f407122092c7791b23a26
Mateusz Szymański <mateusz.szymanski@microsoft.wsei.edu.pl>
2022-03-30 11:24:09 +0200

    lab3 30.03

--- projekt2/index.ts
+++ projekt2/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import {Request, Response} from 'express'
+import fs from 'fs'
 
 const app = express()
 
@@ -12,14 +13,6 @@ interface Note {
   content: string
   createDate: string
   tags?: Tag[]
- 
-  // constructor(n: Note){
-  //   this.id = Date.now()
-  //   this.title = n.title;
-  //   this.content = n.content;
-  //   this.createDate = n.createDate;
-  //   this.tags = n.tags;
-  // }
 }
 
 let notes : Note[] =[
@@ -28,7 +21,9 @@ let notes : Note[] =[
     title: "test",
     content: "this is a test note",
     createDate: "rndDate",
-    // tags: ["tag1"]
+    tags: [
+      {id: 1, name: "test name"}
+    ]
   }
 ]
 
@@ -38,13 +33,13 @@ app.post('/note', function (req: Request, res: Response) {
   const id = req.body.id == null? Date.now(): req.body.id
   const newNote : Note =
   {
-    id : id, 
+    id : id,
     title : req.body.title,
     content : req.body.content,
     createDate : data,
     tags : req.body.tags
   }
-  
+
   //if new tag/tags in notes->tags
   const noteTags = req.body.tags as Tag[]
   noteTags.forEach(element => {
@@ -63,50 +58,49 @@ app.post('/note', function (req: Request, res: Response) {
   if(newNote.title!==null && newNote.content!==null)
   {
     notes.push(newNote);
-    console.log(req.body) 
-    res.sendStatus(201).send(newNote.id)
+    res.send(notes.find(note=>note.id == id)) //sendStatus(201)
   }else{
-    res.sendStatus(400).send("no title or content")
+    res.send("no title or content") //sendStatus(400)
   }
   })
 
 //GET single
 app.get('/note/:id', function (req: Request, res: Response) {
-  const id = parseInt(req.body.id)
-  if(notes.findIndex(note=>note.id == id)){
-    res.sendStatus(200).send(notes.findIndex(note=>note.id == id))
+  const id = +req.params.id
+  if(notes.find(note=>note.id == id) !== undefined) {
+    res.send(notes.find(note=>note.id == id)) //sendStatus(200)
   }else{
-    res.sendStatus(404).send("no object")
+    res.send("no object") //sendStatus(404)
   }
 })
 
 //GET all
-app.get('/note', function (req: Request, res: Response) {
+app.get('/notes', function (req: Request, res: Response) {
   if(notes != null)
-    res.sendStatus(200).send(notes)
+    res.send(notes)
   else
-    res.sendStatus(400).send("notes are empty")
+    res.send("notes are empty") //sendStatus(400)
 })
 
 //PUT
 app.put('/note/:id', function (req: Request, res: Response) {
-  const id = parseInt(req.body.id)
-  if(notes.findIndex(note=>note.id == id)){
+  const id = +req.params.id
+  if(notes.find(note=>note.id == id) !== undefined){
     notes[notes.findIndex(note=>note.id == id)] = req.body;
-    res.sendStatus(200).send(notes.findIndex(note=>note.id == id))
+    res.send(notes.find(note=>note.id == id)) //sendStatus(200)
   }else{
-    res.sendStatus(404).send("no object")
+    res.send("no object") //sendStatus(404)
   }
 })
 
 //DELETE
 app.delete('/note/:id', function(req: Request, res: Response){
-  const id = parseInt(req.body.id)
-  if(notes.find(note=>note.id == id)){
-    res.sendStatus(200).send(notes.findIndex(note=>note.id == id))
+  const id = +req.params.id
+  if(notes.find(note=>note.id == id) !== undefined){
+    res.send(notes.find(note=>note.id == id))
     notes.splice(notes.findIndex(note=>note.id == id),1)
   }else{
-    res.sendStatus(404).send("no object")
+    res.send("no object") //sendStatus(404)
   }
 })
 
@@ -129,16 +123,15 @@ app.post('/tag', function (req: Request, res: Response) {
   const id = req.body.id == null? Date.now(): req.body.id
   const newTag : Tag =
   {
-    id : id, 
+    id : id,
     name : req.body.name
   }
   if(newTag.name!==null)
   {
     tags.push(newTag);
-    console.log(req.body) 
-    res.sendStatus(201).send(newTag.id)
+    res.send(tags.find(tag=>tag.id == id)) //sendStatus(201)
   }else{
-    res.sendStatus(400).send("no name")
+    res.send("no name") //sendStatus(400)
   }
   // req.body.name.forEach(element => {
   //   if(!tags.findIndex(tag=>tag.name === element.name))
@@ -156,45 +149,45 @@ app.post('/tag', function (req: Request, res: Response) {
   //       res.sendStatus(400).send("no name")
   //     }
   // });
-  })
+})
 
 //GET single
 app.get('/tag/:id', function (req: Request, res: Response) {
-  const id = parseInt(req.body.id)
-  if(tags.findIndex(tag=>tag.id == id)){
-    res.sendStatus(200).send(tags.findIndex(tag=>tag.id == id))
+  const id = +req.params.id
+  if(tags.find(tag=>tag.id == id) !== undefined) {
+    res.send(tags.find(tag=>tag.id == id)) //sendStatus(200)
   }else{
-    res.sendStatus(404).send("no object")
+    res.send("no object") //sendStatus(404)
   }
 })
 
 //GET all
 app.get('/tags', function (req: Request, res: Response) {
   if(tags != null)
-    res.sendStatus(200).send(tags)
+    res.send(tags) //sendStatus(200)
   else
-    res.sendStatus(400).send("tags are empty")
+    res.send("tags are empty") //sendStatus(400)
 })
 
 //PUT
 app.put('/tag/:id', function (req: Request, res: Response) {
-  const id = parseInt(req.body.id)
-  if(tags.findIndex(tag=>tag.id == id)){
+  const id = +req.params.id
+  if(tags.find(tag=>tag.id == id) !== undefined){
     tags[tags.findIndex(tag=>tag.id == id)] = req.body;
-    res.sendStatus(200).send(tags.findIndex(tag=>tag.id == id))
+    res.send(tags.find(tag=>tag.id == id)) //sendStatus(200)
   }else{
-    res.sendStatus(404).send("no object")
+    res.send("no object") //sendStatus(404)
   }
 })
 
 //DELETE
 app.delete('/tag/:id', function(req: Request, res: Response){
-  const id = parseInt(req.body.id)
-  if(tags.find(note=>note.id == id)){
-    res.sendStatus(200).send(tags.findIndex(tag=>tag.id == id))
+  const id = +req.params.id
+  if(tags.find(tag=>tag.id == id) !== undefined){
+    res.send(tags.find(tag=>tag.id == id)) //sendStatus(200)
     notes.splice(tags.findIndex(tag=>tag.id == id),1)
   }else{
-    res.sendStatus(404).send("no object")
+    res.send("no object") //sendStatus(404)
   }
 })
 
