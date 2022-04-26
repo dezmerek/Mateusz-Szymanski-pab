f2c498b6a1e36eade35864ea62a9e40f76fb97f5
Mateusz Szymański <mszymanski027@gmail.com>
2022-04-27 01:14:01 +0200

    .

--- projekt2/src/models/note.ts
+++ projekt2/src/models/note.ts
@@ -1,80 +1,21 @@
-import express, { json } from 'express'
-import {Request, Response} from 'express'
+import { ObjectId } from "mongodb";
+import {Tag} from "./tag";
 
-import {UserFile,User,DataStorage} from "../models/user";
-
-const router = express.Router()
-
-router.use((req,res,next)=>{
-    next()
-})
-
-router.post('/', async (req, res) => {
-    const config = await import('../config.json')
-    let user:any;
-    if(config.UseDataBase){
-        user = new User({Login:"123",Password:"123"})
-    }else{
-        user = new UserFile({Login:"123",Password:"123"})
+class Note {
+    id:number
+    UserID:ObjectId
+    title:string
+    content:string
+    createdate:Date
+    tags:Tag[]
+    constructor(args: any) {
+        this.id = args.id
+        this.title = args.title
+        this.content = args.content
+        this.createdate = args.createdate
+        this.tags = args.tags
+        this.UserID = args.UserID
     }
-    setTimeout(() => {
-        user.AddNote(req.body)
-        res.sendStatus(200).send()
-    }, 100)
-    
-})
+}
 
-router.get('/', async function (req: Request, res: Response) {
-    const user = new UserFile({Login:"123",Password:"123"})
-    setTimeout(() => {
-        const data = user.notes       
-        if(data.length >0 ){
-            res.status(200).send(data)
-        }else{
-            res.status(400).send("brak")
-        }
-    }, 100);
-});
-
-router.get('/:id', (req, res) => {
-    const user = new UserFile({Login:"123",Password:"123"})
-    setTimeout(() => {
-        
-        var id = parseInt(req.params.id);
-        user.notes.forEach((element:any) => {
-            if(element.id === id){
-                res.status(200).send(element)
-            }
-        });
-        res.status(404).send()
-    }, 100);
-})
-
-router.put('/:id', function (req: Request, res: Response) {
-    const user = new UserFile({Login:"123",Password:"123"})
-    setTimeout(() => {
-        const result = user.UpdateNote({id:req.body.id,title:req.body.title,content:req.body.content,createdate:new Date,tags:req.body.tags})
-        console.log(result);
-        
-        if(result){
-            res.status(200).send(user.notes)
-        }else{
-            res.status(404).send()
-        }
-    }, 100);
-});
-
-router.delete('/:id', function (req: Request, res: Response) {
-    var id = parseInt(req.params.id);
-    const user = new UserFile({Login:"123",Password:"123"})
-    setTimeout(() => {
-        const result = user.DeleteNote(id)
-        if(result){
-            res.status(200).send()
-        }else{
-            res.status(400).send()
-        }
-    }, 100);
-});
-
-module.exports = router
+export {Note}
--- projekt2/src/models/tag.ts
+++ projekt2/src/models/tag.ts
@@ -7,4 +7,5 @@ class Tag {
     }
 }
 
+
 export {Tag}
